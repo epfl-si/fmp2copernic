8520b35acc86f32bbc10555b489337204a4bd420
Pierre Quinton <pierre.quinton@gmail.com>
2017-03-11 11:58:30 +0100

    serve static protected and parsing xml done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var serveStatic = require('serve-static')
 var morgan = require('morgan')
+var convert = require('xml-js')
+var fs = require('fs')
 
 var app = express()
 
@@ -12,7 +14,14 @@ app.get('/', function (req, res) {
   res.send('Bloub World!')
 })
 
-// to serve static files, we should protect every files except users.xml
-app.use(serveStatic('.', {'index': ['default.html', 'default.htm']}))
+// to serve static files from public_data folder
+app.use(serveStatic('public_data',  {'index': ['index.html', 'index.htm']}))
+
+// parse xml file to be able to use it
+var xml = fs.readFileSync('public_data/users.xml', 'utf8');
+var options = {ignoreText: true, alwaysChildren: true};
+var users = convert.xml2js(xml, options);
+
+
 app.listen(3000)
 
