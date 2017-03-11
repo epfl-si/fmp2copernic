384d21d2e24a3f19eb046ec9446be420c9b62d16
Pierre Quinton <pierre.quinton@gmail.com>
2017-03-11 11:23:13 +0100

    added static serve to the server, now can get users.xml

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express')
+var serveStatic = require('serve-static')
+var morgan = require('morgan')
+
+var app = express()
+app.use(morgan('combined'))
+
+app.get('/', function (req, res) {
+  res.send('Bloub World!')
+})
+
+app.use(serveStatic('.', {'index': ['default.html', 'default.htm']}))
+app.listen(3000)
