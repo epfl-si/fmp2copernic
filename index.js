904e38b67b8b98bdcfc78a81eb70df46dbbe459c
Pierre Quinton <pierre.quinton@gmail.com>
2017-03-11 11:31:24 +0100

    added comment for understandability

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var serveStatic = require('serve-static')
 var morgan = require('morgan')
 
 var app = express()
+
+// for error handling
 app.use(morgan('combined'))
 
+// greeting page, might want to print a help message to use all command possible with the server.
 app.get('/', function (req, res) {
   res.send('Bloub World!')
 })
 
+// to serve static files, we should protect every files except users.xml
 app.use(serveStatic('.', {'index': ['default.html', 'default.htm']}))
 app.listen(3000)
+
