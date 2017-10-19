0fa69ac6263aae239c8c47aabd1374ed2d60f695
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-10-19 10:46:59 +0200

    (WIP) Test rig
    Unloading the whole thing unto Nicolas for now ☺

---
+++ fmp2copernic.js
@@ -0,0 +1,33 @@
+/**
+ * Main server class
+ */
+
+const express = require('express'),
+    _ = require("lodash")
+
+/**
+ * @constructor
+ * @param opts.port TCP port to serve on (0 to let the OS allocate it)
+ * @param opts.copernicHostPort Copernic back-end address in host:port format
+ */
+module.exports = function Fmp2CopernicGateway(opts) {
+    this.opts = _.extend({port: 3000}, opts)
+}
+
+/**
+ * Called by run()
+ */
+Fmp2CopernicGateway.prototype._setupExpress = function (express) {
+    let backendBaseUrl = this.opts.copernicHostPort
+
+    express.get('/copernic/newfact', function (req, res) {
+        res.send('Hello World!')
+    })
+}
+
+/**
+ * Run the proxy server
+ *
+ * @returns {Promise}
+ */
+Fmp2CopernicGateway.prototype.run = require("lib/express-server-mixin.js").run
---
+++ test/mock/copernic.js
@@ -0,0 +1,30 @@
+/**
+ * @constructor
+ */
+const promisify = require("../../lib/promises").promisify;
+
+function MockCopernic() {}
+module.exports = MockCopernic
+
+MockCopernic.prototype._setupExpress = function(app) {
+    let self = this
+    app.post("/piq/RESTAdapter/api/sd/facture", function (req, res) {
+        var payload = {}; // TODO: decode from POSTed JSON
+        promisify(self.handleNewfact(req, payload)).then(function () {
+            // TODO: observe actual success behavior from the real thing
+            res.send('OK')
+        }).catch(function () {
+            // TODO: observe actual error behavior from the real thing
+            res.send('NOT OK')
+        })
+    })
+}
+
+MockCopernic.prototype.run = require("../../lib/express-server-mixin.js").run
+
+MockCopernic.prototype.reset = function() {
+    // Reset methods to their prototype, in case a test overrode them
+    delete this.handleNewFact
+}
+
+MockCopernic.prototype.handleNewFact = function() {}
