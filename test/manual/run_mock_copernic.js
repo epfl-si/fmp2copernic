3535ba4ce7b65fb3a41999f519615c5c347c2658
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-10-24 07:17:59 +0200

    Fix promise logic
    As a reminder to self: .then() always takes a *function*, and promise
    functions return a Promise (or occasionally, a resolved object)

--- test/manual/run_mock_copernic.js
+++ test/manual/run_mock_copernic.js
@@ -1,10 +1,11 @@
 let MockCopernic = require("../mock/copernic.js"),
   mockCopernic = new MockCopernic()
 
-mockCopernic.run({
-  opts: {
-    port: 3000
-  }
-}).then(function(express) {
-  console.log("Mock Copernic running on port " + express.address().port)
+
+mockCopernic.opts = {
+    port: 3010
+}
+
+mockCopernic.run().then(function(express) {
+  console.log("Mock Copernic running on port " + express.listener.address().port)
 })
