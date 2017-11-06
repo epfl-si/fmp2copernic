f39bd027a2d8e0f2716243406007bb62ffcab96f
nicolasreymond <n.reymond.12@gmail.com>
2017-11-06 09:36:04 +0100

    WIP

--- test/mockCopernicTests.js
+++ test/mockCopernicTests.js
@@ -2,22 +2,27 @@ let assert = require("assert"),
   Copernic = require("./mock/copernic.js")
 
 describe.only("tests for MockCopernic", function() {
-  it("serves a success", function() {
-    //start a MockCopernic
-    var mockCopernic = new Copernic;
-    ...;
-    ...;
+  before(function() {
+    fakeCopernic = new Copernic()
+    return fakeCopernic.run().then(function() {
+      port: 0 // Let the OS pick a port
+    })
+  })
 
-    mockCopernic.handleNewfact = function() {
+  it.only("serves a success", function() {
+    //start a MockCopernic
+
+    fakeCopernic.handleNewfact = function() {
       return "12345"
     }
-
-    // use the npm module request to make a request and check the http code should be 200 and Content-Type should be application/json
   })
+
+  // use the npm module request to make a request and check the http code should be 200 and Content-Type should be application/json
   it("serves an error", function() {
 
   })
 })
+
 
 
 // .only for only one describe or it
