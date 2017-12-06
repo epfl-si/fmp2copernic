017eca07040f0df0c6ad6e992d3757f0a9df9b73
nicolasreymond <n.reymond.12@gmail.com>
2017-12-06 10:07:19 +0100

    add test plan for "it transmits the sciper"

--- test/newfact.js
+++ test/newfact.js
@@ -1,6 +1,7 @@
 let assert = require("assert"),
   Copernic = require("./mock/copernic.js"),
-  Fmp2CopernicGateway = require("../fmp2copernic.js")
+  Fmp2CopernicGateway = require("../fmp2copernic.js"),
+  rp = require('request-promise-native')
 
 describe("/copernic/newfact gateway", function() {
   let underTest, fakeCopernic;
@@ -9,7 +10,7 @@ describe("/copernic/newfact gateway", function() {
     return fakeCopernic.run().then(function() {
       underTest = new Fmp2CopernicGateway({
         port: 0, // Let the OS pick a port
-        copernicHostPort: fakeCopernic.getHostPort
+        copernicHostPort: fakeCopernic.getHostPort()
       })
       return underTest.run()
     }).then(function() {
@@ -20,20 +21,34 @@ describe("/copernic/newfact gateway", function() {
   beforeEach(function() {
     fakeCopernic.reset();
   })
+  let uriTest = () => underTest.baseUrl + "/copernic/newfact?ordertype=INTERNE" +
+    "&ordernr=OF-4-2017&currency=CHF&clientnr=243371&fictr=0380" +
+    "&name=FAC-4-2017&sciper=106550&fund=520088&number=9010192" +
+    "&qty=1&price=3140&text=Projet%20:%20test%20Copernic" +
+    "&execmode=SIMU" +
+    "&PathFacturePDF=P:%2FATPR%2FTravaux%2F2017%2FSTI-DO%2FQuatravaux%20Dominique%20Herv%C3%A9%20Claude%2F25.09.2017-OF-4%2FFAC_OF-4-2017.pdf" +
+    "&PathDevisPDF=P:%2FATPR%2FTrav4ux%2F2017%2FSTI-DO%2FQuatravaux%20Dominique%20Herv%C3%A9%20Claude%2F25.09.2017-OF-4%2FDevis_OF-4-2017.pdf"
+
 
   it("decodes and forwards a simple request", function() {
-    let rp = require('request-promise-native');
-    return rp(underTest.baseUrl + "/copernic/newfact?ordertype=INTERNE" +
-        "&ordernr=OF-4-2017&currency=CHF&clientnr=243371&fictr=0380" +
-        "&name=FAC-4-2017&sciper=106550&fund=520088&number=9010192" +
-        "&qty=1&price=3140&text=Projet%20:%20test%20Copernic" +
-        "&execmode=SIMU" +
-        "&PathFacturePDF=P:%2FATPR%2FTravaux%2F2017%2FSTI-DO%2FQuatravaux%20Dominique%20Herv%C3%A9%20Claude%2F25.09.2017-OF-4%2FFAC_OF-4-2017.pdf" +
-        "&PathDevisPDF=P:%2FATPR%2FTravaux%2F2017%2FSTI-DO%2FQuatravaux%20Dominique%20Herv%C3%A9%20Claude%2F25.09.2017-OF-4%2FDevis_OF-4-2017.pdf")
-      .then(function(response) {
-        assert(true)
+    return rp({
+        uri: uriTest(),
+      })
+      .then(responseBody => {
+        assert.equal(responseBody, "OK 12345") // XXX It is actually not clear that FileMaker would be happy with this. Need to confirm with Claude
       })
   })
+  it("transmits the sciper", function() {
+    var sciperInMock = null;
+    // TODO: have the mock record the SCIPER in sciperInMock.
+    return rp({
+        uri: uriTest(),
+      })
+      .then(responseBody => {
+        assert.equal(sciperInMock, 106550)
+      })
+  })
+
 
   after(function() {
     return underTest.shutdown().then(() => fakeCopernic.shutdown())
